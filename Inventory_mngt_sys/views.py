83d5538bf7812c01431b22957123501769d5d5d7
jimmynyasulu98 <jimmymyasulu98@gmail.com>
2022-11-02 23:44:00 +0200

    form validation

--- Inventory_mngt_sys/views.py
+++ Inventory_mngt_sys/views.py
@@ -1,25 +1,42 @@
 import imp
+from unicodedata import category
 from django.shortcuts import render
 from . models import Stock
-from . forms import StockCreateForm
+from . forms import StockCreateForm,StockSearchForm
 
 def home(request):
     
     return render(request , "home.html")
 
 def list_items(request):
+    formsearch = StockSearchForm(request.POST)
     stock = Stock.objects.all()
     context = {
-        "stock" : stock 
-    }
+        "stock"  : stock,
+        "form"   : formsearch,
+     
+        }
+    if request.method == 'POST':
+        search = Stock.objects.filter(category__icontains=formsearch['category'].value()) 
+			
+        
+        context = {
+        "stock"  :  search,
+        "form"   : formsearch,
+        
+        
+      }
+        
+    
     return render(request , "list_items.html", context)
 
 def add_item(request):
     form = StockCreateForm()
     if request.method == 'POST':
-        form = StockCreateForm(request.POST)
+        form = StockCreateForm(request.POST)  
         if form.is_valid():
             form.save()
+          
             
     context = {
         "form" : form
